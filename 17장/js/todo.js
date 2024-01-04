1a3b3cc281bbfb4aba614b3eccefa785e1da194b
BBAKBBU <144880333+BBAKBBU@users.noreply.github.com>
2024-01-04 14:21:05 +0900

    Add files via upload

---
+++ 17장/js/todo.js
@@ -0,0 +1,11 @@
+//웹 요소 가져오기
+const todoInput = document.querySelector('#odo-input');
+const addButton = document.querySelector('#add-Button');
+const todoList = document.querySelector('#todo-List');
+
+//이벤트 처리
+//addButton.addEventListener('click', addTodo);
+
+//함수
+//function addTodo(e){
+//    e.peventDefault();  //기본 동작을 취소합니다
