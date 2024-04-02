2d5d7cc6a1cbaea15dad4355d63387ded963db06
Park BuGeon <144880333+BBAKBBU@users.noreply.github.com>
2024-04-02 11:40:43 +0900

    Delete 5장 directory

--- 5장/js/classlist-2.js
+++
@@ -1,9 +0,0 @@
-const title = document.querySelector("#title");
-
-title.onclick = () => {
-    if(!title.classList.contains("clicked")){
-        title.classList.add("clicked");
-    } else{
-        title.classList.remove("clicked");
-    }
-}
--- 5장/js/darkmode.js
+++
@@ -1,5 +0,0 @@
-const bttn = document.querySelector("button");
-
-bttn.onclick = function(){
-    document.boby.classList.toggle("dark");
-}
