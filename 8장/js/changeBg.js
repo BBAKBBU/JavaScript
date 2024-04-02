0b481fa80088705071ab63203a2779d68889d192
Park BuGeon <144880333+BBAKBBU@users.noreply.github.com>
2024-04-02 11:41:15 +0900

    Delete 8장 directory

--- 8장/js/changeBg.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    const bgCount = 5;
-    let randNum = Math.floor(Math.random()*bgCount + 1);
-    document.body.style.backgroundImage =`ur1(images/bg-${rnadNum}.jpg)`;
-}
