1c52bd11cacc106b6737acb23c07c84b39bbaab5
Park BuGeon <144880333+BBAKBBU@users.noreply.github.com>
2024-04-02 11:40:23 +0900

    Delete 3장 directory

--- 3장/js/conditional-2.js
+++
@@ -1,6 +0,0 @@
-//프롬프트 창에서 {취소}버튼을 클릭했는지 체크하기2
-let userInput = prompt("이름을 입력하세요.");
-
-if(userInput === null){
-    alert(userInput);
-}
--- 3장/js/forof.js
+++
@@ -1,7 +0,0 @@
-//for...of 문으로 배열값 가져오기
-const students = ["Park", "Kim", "Lee", "Kang"];
-
-//students에 student가 있는 동안 계속 반복합니다.
-for(student of students){
-    document.write(`${student}.`);
-}
